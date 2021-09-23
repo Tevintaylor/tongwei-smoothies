7d9c01ca2aa178d465597d62d0237121b5278d9c
Tevintaylor <tevintaylor_1996@yahoo.com>
2021-09-24 02:56:22 +0800

    Index Component.

---
+++ src/components/Index.vue
@@ -0,0 +1,71 @@
+<template>
+
+  <div>
+  <h2 class="main-head blue-text">Smoothies</h2>
+  <div class="index container">
+    <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
+        <div class="card-content">
+          <h2 class="blue-text">
+              {{ smoothie.title }}
+          </h2>
+          <ul class="ingredients">
+              <li v-for="(ingredient,index) in smoothie.ingredients " :key="index">
+                 <span class="chip">
+                    {{ ingredient }}
+                 </span>
+              </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      smoothies:[
+        {
+        title: 'Tongwei Brew',
+        slug: 'tong-wei-brew',
+        ingredients: ['oats','almonds','milk'],
+        id: '1'
+        },
+        {
+        title: 'shanshan Brew',
+        slug: 'shanshan-brew',
+        ingredients: ['mango','plums','milk'],
+        id: '2'
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style >
+.main-head{
+text-align: center;
+}
+ .index{
+    display:grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap:30px;
+    margin-top:60px;
+  }
+
+  .index h2{
+    font-size:1.8em;
+    text-align: center;
+    margin-top:0px;
+  }
+  .index .ingredients {
+    margin: 30px auto;
+  }
+  .index .ingredients li{
+    display: inline-block;
+  }
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="navbar">
+    <nav class="navbar-extended blue darken-2">
+      <div class="nav-content">
+         <!-- <img src="https://media.istockphoto.com/vectors/smoothie-flat-design-naturopathy-icon-with-side-shadow-vector-id925601864?b=1&k=20&m=925601864&s=612x612&w=0&h=g1uIYIYlWLwGG5nfYXBdlWO1t5WTDy4g3zQ3KUifkWo="> -->
+          <router-link to="">
+              <span class="nav-title"> Tong Wei 佟维 Smoothies</span>
+          </router-link>
+          <a href="" class="btn-floating btn-large halfway-fab green">
+             <router-link to="">
+                <i class="material-icons">add</i>
+             </router-link>
+          </a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+.navbar nav
+{
+  padding:0 20px;
+  height: 20%;
+  }
+
+  img{
+  height: 20%;
+  width:20%;
+  }
+</style>
